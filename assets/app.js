f49144eaef5bd98dff83f990a13b42a24ad14777
Sabrina <zsbrina20@gmail.com>
2019-02-07 22:08:22 -0600

    first push

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+$('.parallax').parallax();
+ 
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 60,
+    loop: true,
+    backSpeed: 40,
+    showCursor: false,
+  });
